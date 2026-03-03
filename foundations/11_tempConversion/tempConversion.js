e1cf1d740f98aaa79b7fcfc0ff7ecee29870d390
Jenfer Renton <jeppoyt@gmail.com>
2026-03-03 23:38:56 +0800

    finished 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function (temp) {
+  const newTemp = temp * (9 / 5) + 32;
+  return Number.isInteger(newTemp)
+    ? Math.trunc(newTemp)
+    : Number(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  const newTemp = (temp - 32) * (5 / 9);
+  return Number.isInteger(newTemp)
+    ? Math.trunc(newTemp)
+    : Number(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
