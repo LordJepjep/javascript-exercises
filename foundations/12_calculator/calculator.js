0e272afe76f29fbdf914ca393958d96c68acbe18
Jenfer Renton <jeppoyt@gmail.com>
2026-04-16 00:10:39 +0800

    finish 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((product, num) => product *= num, 1); 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = base;
+	for(i = 1; i < exponent; i++){
+    result *= base;
+  }
+  console.log(result);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for(i = 1; i <= num; i++){
+    result *= i;
+  }
+  console.log(result);
+  return result;
 };
 
 // Do not edit below this line
