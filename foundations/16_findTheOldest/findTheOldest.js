575576d166465f9c785283020d9084dad7668f22
Jenfer Renton <jeppoyt@gmail.com>
2026-04-17 00:17:54 +0800

    finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.reduce((oldest, person) => {
+    const getAge = (person) => {
+      const yearOfBirth = person.yearOfBirth;
+      let yearOfDeath = person.yearOfDeath;
+      if (yearOfDeath === undefined) yearOfDeath = new Date().getFullYear();
+      return yearOfDeath - yearOfBirth;
+    };
 
+    if (oldest === null) return person;
+    const currentOldestAge = getAge(oldest);
+    const personAge = getAge(person);
+    return currentOldestAge > personAge ? oldest : person;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
