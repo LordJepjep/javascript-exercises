13a64da89e967ba033f14000b6ad6a77f22e5d29
Jenfer Renton <jeppoyt@gmail.com>
2026-04-16 23:30:32 +0800

    finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (steps) {
+  const stepsNum = Number(steps);
+  if (stepsNum < 0) return "OOPS";
+  if (stepsNum === 0) return 0;
+  let result = 0,
+    counter = 1;
+  let firstNum = 0,
+    secondNum = 1;
 
+  do {
+    result = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = result;
+    counter++;
+  } while (counter < stepsNum);
+  return result;
 };
 
 // Do not edit below this line
