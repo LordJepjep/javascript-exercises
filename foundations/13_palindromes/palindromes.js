f0b352841cf0a7e24364ff861b3fc0d915fb4dbe
Jenfer Renton <jeppoyt@gmail.com>
2026-04-16 22:48:36 +0800

    finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+const cleanedText = text
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .split("")
+    .join("");
+  const reversedText = cleanedText.split("").reverse().join("");
+  return cleanedText === reversedText ? true : false;
 };
 
 // Do not edit below this line
